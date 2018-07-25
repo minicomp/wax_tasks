cebfaa3346953857d091b83bb741da3069df276a
marii¨ <mnyrop@users.noreply.github.com>
2018-07-25 18:04:24 -0400

    format [skip ci]

--- lib/wax_tasks.rb
+++ lib/wax_tasks.rb
@@ -8,30 +8,28 @@ require_relative 'wax_tasks/pagemaster_collection'
 require_relative 'wax_tasks/task_runner'
 require_relative 'wax_tasks/utils'
 
-# WaxTasks is the module powering the Rake tasks in `./tasks`
-# including:
+# The WaxTasks module powers the Rake tasks in `./tasks`, including:
 #
-# wax:pagemaster :: generate collection md pages from yaml or csv data source
-# wax:lunr :: build lunr search index (with default UI if UI=true)
-# wax:iiif :: generate iiif derivatives from local image files
-# wax:jspackage :: write a simple package.json for monitoring js dependencies
-# wax:push :: push compiled Jekyll site to git branch
-# wax:test :: run htmlproofer, rspec if .rspec file exists
+# wax:pagemaster  :: generate collection md pages from csv, json, or yaml data file
+# wax:lunr        :: build lunr search index (with default UI if UI=true)
+# wax:iiif        :: generate iiif derivatives from local image files
+# wax:jspackage   :: write a simple package.json for monitoring js dependencies
+# wax:push        :: push compiled Jekyll site to git branch
+# wax:test        :: run htmlproofer, rspec if .rspec file exists
 #
-# Each task is run by a WaxTasks::TaskRunner object which is resposible extend
-# for reading in site site config from `_config.yml`
+# Tasks are run by a WaxTasks::TaskRunner object which is resposible
+# for reading in site config from `_config.yml`
 module WaxTasks
+  # ----------
   # CONSTANTS
+  # ----------
 
   # @return [String] The path to load Jekyll site config
   DEFAULT_CONFIG  = '_config.yml'.freeze
-
   # @return [String] The path to write WaxTasks::LunrIndex
   LUNR_INDEX_PATH = 'js/lunr_index.json'.freeze
-
   # @return [String] The path to write default LunrUI
   LUNR_UI_PATH    = 'js/lunr_ui.js'.freeze
-
   # @return [String] The path to the compiled Jekyll site
   SITE_DIR        = './_site'.freeze
 end
